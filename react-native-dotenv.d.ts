1a723b03d4db6ab17149e512137a452dc9ecf0ae
Yeonhan Jung <yt4307@gmail.com>
2023-11-01 20:18:25 +0900

    init: 초기 커밋

---
+++ react-native-dotenv.d.ts
@@ -0,0 +1,7 @@
+declare module "@env" {
+  export const SERVER_URL: string;
+  export const DEVELOP_SERVER_URL: string;
+  export const DEVELOP_MODE: string;
+  export const API: string;
+  export const KAKAO_REST_API_KEY: string;
+}
