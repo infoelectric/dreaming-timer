b6ed9da62326080ef746801c25a8f2c580da6986
jeongjoonhee <wjdwnsgml@gamil.com>
2023-11-04 16:42:22 +0900

    feat. 물 마시기 창 완성!

--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -1,4 +1,5 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import DrinkingScreen from "@screens/missons/DrinkingScreen";
 import SelectionScreen from "@screens/missons/SelectionScreen";
 import TongueTwisterScreen from "@screens/missons/TongueTwisterScreen";
 
@@ -9,6 +10,7 @@ const StackNavigator = () => {
     <Stack.Navigator screenOptions={() => ({ headerShown: false })}>
       <Stack.Screen name="미션선택" component={SelectionScreen} />
       <Stack.Screen name="잰말놀이" component={TongueTwisterScreen} />
+      <Stack.Screen name="물 마시기" component={DrinkingScreen} />
     </Stack.Navigator>
   );
 };
